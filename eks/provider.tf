18fc73fe1215ef8e36c9d8c11fa944501ebd396c
dharma0907 <83895329+dharma0907@users.noreply.github.com>
2024-07-28 15:13:18 +0530

    Update provider.tf

--- eks/provider.tf
+++ eks/provider.tf
@@ -1,6 +1,15 @@
 //we are creating provider in aws along with region
 terraform {
   required_providers {
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
+terraform {
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
