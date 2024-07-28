015b9f03c536ef824752154e445a4bdd6017283c
dharma0907 <83895329+dharma0907@users.noreply.github.com>
2024-07-28 15:15:24 +0530

    Update provider.tf

--- eks/provider.tf
+++ eks/provider.tf
@@ -8,14 +8,14 @@ terraform {
   }
 }
 
-terraform {
+/*terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
-}
+}*/
 
 # Configure the AWS Provider
 provider "aws" {
